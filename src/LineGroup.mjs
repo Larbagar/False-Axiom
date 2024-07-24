df4ae3e5e5a8be1c6c748cbe2781ae479c92b4ea
Larbagar <darcyvilkner@gmail.com>
2024-07-24 18:32:28 -0400

    Started working on main menu, refactors to 2-dimensional vector class

--- src/LineGroup.mjs
+++ src/LineGroup.mjs
@@ -1,6 +1,7 @@
 import {device} from "./graphics/device.mjs"
 import {lightLine} from "./graphics/light/lightLine.mjs"
-import {identityBindGroup} from "./graphics/transformMatrixBindGroupLayout.mjs"
+import {identityBindGroup, transformMatrixBindGroupLayout} from "./graphics/transformMatrixBindGroupLayout.mjs"
+import M3 from "./M3.mjs"
 
 class LineGroup {
     /** @type {number} */
@@ -14,10 +15,20 @@ class LineGroup {
     /** @type {GPUBuffer} */
     colBuffer
 
+    /** @type {boolean} */
+    useTransform = false
+    /** @type {M3} */
+    transform = null
+    /** @type {GPUBuffer} */
+    transformBuffer = null
+    /** @type {GPUBindGroup} */
+    transformBindGroup
+
     posChanged = false
     colChanged = false
+    transformChanged = false
 
-    constructor(count) {
+    constructor(count, useTransform = false) {
         this.count = count
         this.pos = new Float32Array(4*this.count)
         this.posBuffer = device.createBuffer({
@@ -31,12 +42,37 @@ class LineGroup {
             size: this.col.byteLength,
             usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
         })
+        if(useTransform){
+            this.useTransform = true
+            this.transformBuffer = device.createBuffer({
+                label: "line group transform matrix buffer",
+                size: M3.BYTE_LENGTH,
+                usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+            })
+            this.transformBindGroup = device.createBindGroup({
+                label: "line group transform matrix bind group",
+                layout: transformMatrixBindGroupLayout,
+                entries: [
+                    {
+                        binding: 0,
+                        resource: {
+                            buffer: this.transformBuffer
+                        },
+                    },
+                ],
+            })
+        }else{
+            this.transformBindGroup = identityBindGroup
+        }
     }
     updatePosBuffer(){
         device.queue.writeBuffer(this.posBuffer, 0, this.pos)
     }
     updateColBuffer(){
         device.queue.writeBuffer(this.colBuffer, 0, this.col)
+    }
+    updateTransformBuffer(){
+        device.queue.writeBuffer(this.transformBuffer, 0, this.transform.arr)
     }
     draw(encoder, lightTex, cameraBindGroup, minBrightnessBindGroup){
         if(this.posChanged){
@@ -47,7 +83,11 @@ class LineGroup {
             this.updateColBuffer()
             this.colChanged = false
         }
-        lightLine(encoder, lightTex, cameraBindGroup, identityBindGroup, this.posBuffer, this.colBuffer, minBrightnessBindGroup, this.count)
+        if(this.transformChanged){
+            this.updateTransformBuffer()
+            this.transformChanged = false
+        }
+        lightLine(encoder, lightTex, cameraBindGroup, this.transformBindGroup, this.posBuffer, this.colBuffer, minBrightnessBindGroup, this.count)
     }
 }
 
