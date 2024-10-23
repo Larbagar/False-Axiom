38821419eeb33480d0efdce84fdb06caea34039b
Larbagar <darcyvilkner@gmail.com>
2024-10-22 21:51:11 -0400

    Added instructions

--- src/start.mjs
+++ src/start.mjs
@@ -13,6 +13,18 @@ handleHistory()
 
 // playConfigSoundtrack()
 
+alert(`\
+This game only works on touchscreens.
+To enter game setup, press the white play button.
+In game setup, create a player by tapping two fingers and spreading them apart. The line that appears is your control zone. Create one control zone for each player.
+The game begins when all players have indicated that they are ready by pressing two fingers on either side of their control zone (but not touching the line).
+
+Once the game has started, players can steer their ships by tapping on the right or left side of their control zone (now hidden).
+Players can dash by tapping on both sides of the control zone simultaneously.
+Each player's health is indicated with dots above their ship. Collisions will inflict damage.
+The goal is to be the last player standing!\
+`)
+
 history.replaceState(states.TITLE, "",)
 document.title = "False Axiom - Title"
 
